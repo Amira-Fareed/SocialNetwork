7d3e8dbecc46fac741f5c13fe261c3242930989c
Emanashraaf <33792561+Emanashraaf@users.noreply.github.com>
2018-05-13 19:18:40 -0700

    Update groups.php

--- groups.php
+++ groups.php
@@ -28,27 +28,13 @@ if(isset($_POST['Creategroup']))
 }
 ?>
 
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>friends</title>
-</head>
-<body>
-
-</body>
-</html>
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Social Network</title>
+    <title>Groups</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="assets/css/Footer-Dark.css">
@@ -61,69 +47,7 @@ if(isset($_POST['Creategroup']))
 </head>
 
 <body onload="showModal()">
-
-    <header class="hidden-sm hidden-md hidden-lg">
-        <div class="searchbox">
-            <form validate method="post">
-                <h1 class="text-left">Social Network</h1>
-                <div class="searchbox"><i class="glyphicon glyphicon-search"></i>
-                    <input class="form-control sbox" type="text"  name="txtInput">
-                    <ul class="list-group autocomplete" style="position:absolute;width:100%; z-index: 100">
-                    </ul>
-                </div>
-                <div class="dropdown">
-                    <button class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button">MENU <span class="caret"></span></button>
-                    <ul class="dropdown-menu dropdown-menu-right" role="menu">
-                        <li role="presentation"><a href="profile.php">My Profile</a></li>
-                        <li role="presentation"><a href="timeline.php">Timeline</a></li>
-                        <li class="divider" role="presentation"></li>
-                        <li role="presentation"><a href="logout.php">Logout </a></li>
-                    </ul>
-                </div>
-            </form>
-        </div>
-        <hr>
-    </header>
-    <div>
-        <nav class="navbar navbar-default hidden-xs navigation-clean">
-            <div class="container">
-                <div class="navbar-header"><a class="navbar-brand navbar-link" href="timeline.php"><i class="icon ion-ios-navigate"></i></a>
-                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                </div>
-                <div class="collapse navbar-collapse" id="navcol-1">
-                    <form class="navbar-form navbar-left" validate method="post">
-                        <div class="searchbox"><i class="glyphicon glyphicon-search"></i>
-                            <input class="form-control sbox" type="text"  name="txtInput">
-                            <ul class="list-group autocomplete" style="position:absolute;width:100%; z-index:100">
-                            </ul>
-                        </div>
-                    </form>
-                    <ul class="nav navbar-nav hidden-md hidden-lg navbar-right">
-                        <li  role="presentation"><a href="timeline.php">Timeline</a></li>
-                        <li class="dropdown open"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true" href="#">User <span class="caret"></span></a>
-                            <ul class="dropdown-menu dropdown-menu-right" role="menu">
-                                <li role="presentation"><a href="profile.php">My Profile</a></li>
-                                <li class="divider" role="presentation"></li>
-                                <li role="presentation"><a href="logout.php">Logout </a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav hidden-xs hidden-sm navbar-right">
-                        <li role="presentation"><a href="timeline.php">Timeline</a></li>
-                        <?php 
-                            if ($currentUSerID != $otherUserId)
-                                echo'<li role="presentation"><a href="profile.php">Profile</a></li>';
-
-                            else
-                                echo'<li class="active" role="presentation"><a href="profile.php">Profile</a></li>';
-                        ?>
-                        
-                        <li role="presentation"><a href="logout.php">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <?php include "header.php";?>
     <div class="container" style="margin-bottom: 60px; margin-top: 20px;">
 
             <div class="row">
